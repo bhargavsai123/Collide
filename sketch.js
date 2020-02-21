26fef17cbc89b4cbb47876dfdcbd3f2752a47e56
bhargavsai123 <61113181+bhargavsai123@users.noreply.github.com>
2020-02-21 17:56:18 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+var movingrect,fixedrect;
+
+
+function setup() {
+  createCanvas(800,400);
+  fixedrect=createSprite(400, 200, 50, 50);
+    fixedrect.shapeColor="blue";
+  movingrect=createSprite(500, 200, 50, 50);
+    movingrect.shapeColor="green";
+}
+
+function draw() {
+  background(255,255,255);  
+
+  movingrect.x=World.mouseX;
+  movingrect.y=World.mouseY;
+
+  if(movingrect.x-fixedrect.x< movingrect.width/2+fixedrect.width/2
+    && fixedrect.x-movingrect.x< movingrect.width/2+fixedrect.width/2
+    && fixedrect.y-movingrect.y<movingrect.height/2+fixedrect.height/2
+    && movingrect.y-fixedrect.y<movingrect.height/2+fixedrect.height/2){
+
+      fixedrect.shapeColor="red";
+      movingrect.shapeColor="red";
+
+    }else{
+
+      fixedrect.shapeColor="blue";
+      movingrect.shapeColor="green";
+    }
+
+
+  drawSprites();
+}
